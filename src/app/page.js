c2b8dcedb975e87e6bbe2d69ee94712fa6ac79da
Line Madsen <lima0001@stud.kea.dk>
2025-11-13 09:57:16 +0100

    components

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,23 @@
 import Image from "next/image";
+import CategoryList from "./components/CategoryList";
+import { Suspense } from "react";
+import ProductList from "./components/listview/ProductList";
+import Header from "./components/Header";
+
 
 export default function Home() {
   return (
- 
+    <div>
+ <Header />
+ <CategoryList />
+ <Suspense >
+  <ProductListContainer />
+ </Suspense>
+ </div>
   );
+}
+
+async function ProductListContainer({ searchParams }) {
+  const { category } = await searchParams;
+  return <List category={category} />;
 }
