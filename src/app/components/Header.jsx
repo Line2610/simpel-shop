eba241974b6ce1e309e3b56f37f864e0cf79a6b4
Line Madsen <lima0001@stud.kea.dk>
2025-11-13 11:19:17 +0100

    header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 
 const Header = () => {
-    return ( 
-        <div className="">
-            <p>Home</p>
-            <p>Products</p>
-        </div>
-     );
-}
- 
+  return (
+    <Link href="/ProductList">
+      <div className="position-sticky top-0 m-2 flex justify-center gap-10 bg-stone-400/70 p-5">
+        <p className="cursor-pointer text-s text-white">Home</p>
+        <p className="cursor-pointer text-s text-white">Products</p>
+      </div>
+    </Link>
+  );
+};
+
 export default Header;
