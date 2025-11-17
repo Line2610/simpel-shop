153574ed80d63d2b6cf530cb71fb331db6aebc69
Tamara Ujmajuridze <Tauj0001@stud.kea.dk>
2025-11-17 11:49:13 +0100

    category og button

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,20 @@
 import Image from "next/image";
-import CategoryList from "./components/CategoryList";
-import { Suspense } from "react";
-import ProductList from "./components/listview/ProductList";
 import Header from "./components/Header";
+import Link from "next/link";
 
 export default async function Home({ searchParams }) {
   return (
     <div>
       <Header />
-      <CategoryList />
       <div className="flex min-h-screen flex-col items-center justify-center">
         <h1 className="mb-8 text-center font-mono text-8xl font-bold">
           Simple Shop
         </h1>
-        <button className="cursor-pointer flex-row rounded-full bg-pink-200 p-4 text-2xl text-gray-700 hover:scale-105 hover:transform">
-          Se produkter
-        </button>
+        <Link href="/products">
+          <button className="cursor-pointer flex-row rounded-full bg-pink-200 p-4 text-2xl text-gray-700 hover:scale-105 hover:transform">
+            Se produkter
+          </button>
+        </Link>
       </div>
     </div>
   );
---
+++ src/app/products/page.js
@@ -0,0 +1,14 @@
+import ProductList from "../components/listview/ProductList";
+import Header from "../components/Header";
+import CategoryList from "../components/CategoryList";
+
+export default async function ProductsPage({ searchParams }) {
+  return (
+    <div>
+      <Header />
+      <CategoryList />
+      <h1 className="mt-15 mb-15 text-center text-4xl font-medium">Products</h1>
+      <ProductList />
+    </div>
+  );
+}
