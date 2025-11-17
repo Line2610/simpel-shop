7a8cd7dcf159148c4acfba2e47a47f12d4853439
Line Madsen <lima0001@stud.kea.dk>
2025-11-17 11:35:38 +0100

    products

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,14 +1,16 @@
-
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <Link href="/ProductList">
-      <div className="position-sticky top-0 m-2 flex justify-center gap-10 bg-stone-400/70 p-5">
-        <p className="cursor-pointer text-s text-white">Home</p>
-        <p className="cursor-pointer text-s text-white">Products</p>
-      </div>
-    </Link>
+    <div className="position-sticky top-0 m-2 flex justify-center gap-10 bg-stone-400/70 p-5">
+      <Link href="/">
+        <p className="text-s cursor-pointer text-white">Home</p>
+      </Link>
+
+      <Link href="/products">
+        <p className="text-s cursor-pointer text-white">Products</p>
+      </Link>
+    </div>
   );
 };
 
