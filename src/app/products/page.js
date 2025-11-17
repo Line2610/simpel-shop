cc79681bad765e68b934750fab0b2ea92911bdf2
Tamara Ujmajuridze <Tauj0001@stud.kea.dk>
2025-11-17 11:44:31 +0100

    categorylist

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,11 +1,15 @@
 import ProductList from "../components/listview/ProductList";
 import Header from "../components/Header";
+import CategoryElement from "../components/CategoryElement";
+import CategoryList from "../components/CategoryList";
 
 export default async function ProductsPage({ searchParams }) {
   return (
     <div>
       <Header />
+      <CategoryList />
       <h1 className="mt-15 mb-15 text-center text-4xl font-medium">Products</h1>
+
       <ProductList />
     </div>
   );
